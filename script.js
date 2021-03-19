dfd5c229d1eb3b56ee257e3e386aaacafcb22e50
Никита <76733643+pancushot@users.noreply.github.com>
2021-03-19 23:08:16 +1000

    Add files via upload
    Simple calculator,with a beautiful background)

---
+++ script.js
@@ -0,0 +1,59 @@
+var a,b,result// переменные которые будут задействоваться 
+
+const sum=()=>{
+   
+        a=document.getElementById('n1').value//получение элемента по его id. Получение  элемента как строки а затем вывод как числа
+        a=parseInt(a);// взяти из строки число 
+
+        b=document.getElementById('n2').value// тоже самое только со вторым и последующие действия аналогичны
+        b=parseInt(b);
+
+        result=a+b
+        
+document.getElementById('out').innerHTML=result// вывод и получение ожидаемого  результата
+
+      
+}
+
+
+const sub=()=>{
+
+        a=document.getElementById('n1').value
+        a=parseInt(a);
+
+        b=document.getElementById('n2').value;
+        b=parseInt(b);
+
+        result=a-b
+
+document.getElementById('out').innerHTML=result
+
+}
+
+
+const mul=()=>{
+     a=document.getElementById('n1').value
+     a=parseInt(a)
+
+     b=document.getElementById('n2').value
+     b=parseInt(b)
+
+     result=a*b
+
+document.getElementById('out').innerHTML=result
+
+}
+
+
+const div=()=>{
+     a=document.getElementById('n1').value
+     a=parseInt(a)
+
+     b=document.getElementById('n2').value
+     b=parseInt(b)
+
+     result=a/b 
+
+document.getElementById('out').innerHTML=result 
+
+}
